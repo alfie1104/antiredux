532502dc0a46549814aaff4746242985b8868cd7
peter_alfie <alfie1104@gmail.com>
2019-12-26 08:52:23 +0900

    Finish

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -3,21 +3,55 @@ import AppPresenter from "./AppPresenter";
 import Store from "store";
 
 class AppContainer extends Component {
-  state = {
-    message: "Hello"
-  };
-
-  componentDidMount = () => {
-    setTimeout(() => {
-      this.setState({
-        message: "Bye"
+  constructor(props) {
+    super(props);
+    this._deleteNotification = id => {
+      this.setState(currentState => {
+        const newState = delete currentState.notifications[id];
+        return newState;
       });
-    }, 2000);
-  };
+    };
+
+    this._seeNotification = id => {
+      this.setState(currentState => {
+        return {
+          ...currentState,
+          notifications: {
+            ...currentState.notifications,
+            [id]: {
+              ...currentState.notifications[id],
+              seen: true
+            }
+          }
+        };
+      });
+    };
+    this.state = {
+      notifications: {
+        "1": {
+          id: 1,
+          text: "Something",
+          seen: false
+        },
+        "2": {
+          id: 2,
+          text: "Something else",
+          seen: false
+        },
+        "3": {
+          id: 3,
+          text: "Something else but different",
+          seen: false
+        }
+      },
+      deleteNotification: this._deleteNotification,
+      seeNotification: this._seeNotification
+    };
+  }
 
   render() {
     return (
-      <Store.Provider value={this.state.message}>
+      <Store.Provider value={this.state}>
         <AppPresenter />
       </Store.Provider>
     );
--- src/Components/Notification/NotificationPresenter.js
+++ src/Components/Notification/NotificationPresenter.js
@@ -55,17 +55,29 @@ const Button = styled.button`
 const NotificationPresenter = ({ id, text, seen }) => (
   <Notification seen={seen}>
     <Flex alignCenter justifyBetween>
-      <Title>
-        <Store.Consumer>{store => JSON.stringify(store)}</Store.Consumer>
-      </Title>
+      <Title>{text}</Title>
       <FlexItem>
         <Fragment>
-          <Button success seen={seen} onClick={() => {}}>
-            <FontAwesome name="check" />
-          </Button>
-          <Button danger seen={seen} onClick={() => {}}>
-            <FontAwesome name="times" />
-          </Button>
+          <Store.Consumer>
+            {store => (
+              <Fragment>
+                <Button
+                  success
+                  seen={seen}
+                  onClick={() => store.seeNotification(id)}
+                >
+                  <FontAwesome name="check" />
+                </Button>
+                <Button
+                  danger
+                  seen={seen}
+                  onClick={() => store.deleteNotification(id)}
+                >
+                  <FontAwesome name="times" />
+                </Button>
+              </Fragment>
+            )}
+          </Store.Consumer>
         </Fragment>
       </FlexItem>
     </Flex>
